d1ba4e710f749fd9c1a1bf237b54e0a4f7a8cc9c
Juan Cas <juancarloscs2010@gmail.com>
2024-08-22 17:45:06 -0500

    posible soluciones que hice para las funciones encriptar y desencriptar

--- js/funciones.js
+++ js/funciones.js
@@ -1,100 +1,27 @@
+//funciones encriptar y descencriptar
 
-
-
-
-// let vocal;
-// let vocales;
-// switch(vocales){
-//     case 1: "a"== "enter";
-//     break;
-//     case 2: "e" == "imes";
-//     break;
-//     case 3: "i"== "ai";
-//     break;
-//     case 4: "o" == "ober";
-//     break;
-//     case 5: "u"== "ufat";
-//     break;
-// }
-
-// function encriptar(){
-//     let txtdelusuario = document.getElementById('txt1').value;
-//     vocales = txtdelusuario.split("");
-    
-//     // let reemp = vocales;
-//     // switch(reemp){
-//     //     case 1: "a"== "enter";
-//     //     break;
-//     //     case 2: "e" == "imes";
-//     //     break;
-//     //     case 3: "i"== "ai";
-//     //     break;
-//     //     case 4: "o" == "ober";
-//     //     break;
-//     //     case 5: "u"== "ufat";
-//     //     break;
-    
-//     // }
-//     reemp="";
-
-//     for (let i = 0; i < vocales.length; i++){
-        
-//         if (vocales[i] == "a"){
-//             vocales[i]= vocales[i].replace("a","enter");
-//         }else if(vocales[i]== "e"){
-//             vocales[i]= vocales[i].replace("e","imes");
-//         }else if(vocales[i]== "i"){
-//             vocales[i] = vocales[i].replace("i","ai");
-//         }else if(vocales[i]=="o"){
-//             vocales[i]= vocales[i].replace("o","ober");
-//         }else if(vocales[i]== "u"){
-//             vocales[i] = vocales[i].replace("u","ufat");
-//         }
-       
-//         reemp += vocales[i];
-//         // let encriptados = new Array(vocales[i])
-//         // let txt = encriptados.join("");
-        
-        
-        
-//     }
-    
-//     console.log(reemp);
-//     document.getElementById("txt2").value = reemp;
-// }
-function desencriptar(){
+function encriptar(){
     let txtdelusuario = document.getElementById('txt1').value;
+
+    // Aquí use split() que  la cadena se divide en un array donde cada elemento 
+    // es un carácter individual de la cadena original.
     vocales = txtdelusuario.split("");
     
-    // let reemp = vocales;
-    // switch(reemp){
-    //     case 1: "a"== "enter";
-    //     break;
-    //     case 2: "e" == "imes";
-    //     break;
-    //     case 3: "i"== "ai";
-    //     break;
-    //     case 4: "o" == "ober";
-    //     break;
-    //     case 5: "u"== "ufat";
-    //     break;
     
-    // }
     reemp="";
 
     for (let i = 0; i < vocales.length; i++){
-        console.log(vocales[i]);
         
-        if (vocales[i] == "enter"){
-            vocales[i]= vocales[i].replace("enter","a");
-        }else if(vocales[i]== "imes"){
-            vocales[i]= vocales[i].replace("imes","e");
-        }else if(vocales[i]== "ai"){
-            vocales[i] = vocales[i].replace("ai","i");
-        }else if(vocales[i]=="ober"){
-            vocales[i]= vocales[i].replace("ober","o");
-        }else if(vocales[i]== "ufat"){
-            vocales[i] = vocales[i].replace("ufat","u");
+        if (vocales[i] == "a"){
+            vocales[i]= vocales[i].replace("a","enter");
+        }else if(vocales[i]== "e"){
+            vocales[i]= vocales[i].replace("e","imes");
+        }else if(vocales[i]== "i"){
+            vocales[i] = vocales[i].replace("i","ai");
+        }else if(vocales[i]=="o"){
+            vocales[i]= vocales[i].replace("o","ober");
+        }else if(vocales[i]== "u"){
+            vocales[i] = vocales[i].replace("u","ufat");
         }
        
         reemp += vocales[i];
@@ -107,4 +34,24 @@ function desencriptar(){
     
     console.log(reemp);
     document.getElementById("txt2").value = reemp;
+}
+function desencriptar(){
+    let txtdelusuario = document.getElementById('txt1').value;
+   
+    //en esta parte investigue sobre las expresiones regulares y aisle los textos a remmplazas : (/enter/g)
+    //  y asi susesivamente para las demas donde la g indica que es global, es decir, reemplazará todas 
+    //las apariciones de la cadena "enter" en el texto, no solo la primera.
+    // Como use arriba use el  replace() a cada llamada toma el resultado del reemplazo 
+    //anterior y aplica el siguiente.
+    txtdelusuario = txtdelusuario
+    .replace(/enter/g, "a")
+    .replace(/imes/g, "e")
+    .replace(/ai/g, "i")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+   
+    
+    console.log(txtdelusuario);
+    
+    document.getElementById("txt2").value = txtdelusuario;
 }
