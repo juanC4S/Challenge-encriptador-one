825400cf6bc8d9c370d27e878c6ceb742b6ed588
Juan Cas <juancarloscs2010@gmail.com>
2024-08-22 17:55:35 -0500

    correcion de letras aeiou me confundi xd

--- js/funciones.js
+++ js/funciones.js
@@ -13,11 +13,11 @@ function encriptar(){
     for (let i = 0; i < vocales.length; i++){
         
         if (vocales[i] == "a"){
-            vocales[i]= vocales[i].replace("a","enter");
+            vocales[i]= vocales[i].replace("a","ai");
         }else if(vocales[i]== "e"){
-            vocales[i]= vocales[i].replace("e","imes");
+            vocales[i]= vocales[i].replace("e","enter");
         }else if(vocales[i]== "i"){
-            vocales[i] = vocales[i].replace("i","ai");
+            vocales[i] = vocales[i].replace("i","imes");
         }else if(vocales[i]=="o"){
             vocales[i]= vocales[i].replace("o","ober");
         }else if(vocales[i]== "u"){
@@ -38,15 +38,16 @@ function encriptar(){
 function desencriptar(){
     let txtdelusuario = document.getElementById('txt1').value;
    
-    //en esta parte investigue sobre las expresiones regulares y aisle los textos a remmplazas : (/enter/g)
-    //  y asi susesivamente para las demas donde la g indica que es global, es decir, reemplazará todas 
-    //las apariciones de la cadena "enter" en el texto, no solo la primera.
-    // Como use arriba use el  replace() a cada llamada toma el resultado del reemplazo 
-    //anterior y aplica el siguiente.
+    // En esta parte investigue sobre las expresiones regulares y aisle los textos a 
+    // remmplazar -> (/enter/g) y asi susesivamente para las demas donde la g indica 
+    // que es global, es decir, reemplazará todas las apariciones de la cadena "enter" 
+    // en el texto, no solo la primera. Como use arriba use el  replace() a cada 
+    // llamada toma el resultado del reemplazo anterior y aplica el siguiente.
+
     txtdelusuario = txtdelusuario
-    .replace(/enter/g, "a")
-    .replace(/imes/g, "e")
-    .replace(/ai/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
     .replace(/ober/g, "o")
     .replace(/ufat/g, "u");
    
