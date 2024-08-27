2b92b5e7de113e01f1c2962a953be903ef440673
Juan Cas <juancarloscs2010@gmail.com>
2024-08-27 01:38:16 -0500

    version terminada

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+// document.addEventListener("DOMContentLoaded", function() {
     
-});
+// });
 
 // definiendp variables con los clases y ids del html y expresiones regulares
 const txtdelusuario = document.getElementById('txt1');
@@ -8,6 +9,7 @@ const mensaje = document.getElementById("txt2");
 const textArea = document.querySelector(".mensaje"); 
 const muñeco = document.querySelector(".muñecoPng");
 const miniinfo = document.querySelector(".informacion");
+
 // Expresión regular para validar la entrada ya que permite solo letras 
 // sin acentos, números y espacios
 const regex = /[^a-zA-Z0-9\s]/g;
@@ -15,10 +17,13 @@ const regex = /[^a-zA-Z0-9\s]/g;
 
 const spam = document.querySelector(".Alerta");
 
+
+
+
 function btnEncriptar(){
 
+   
     const textEncriptado = Encriptar(txtdelusuario.value.replace(regex, ""));
-    
     mensaje.value = textEncriptado;
     txtdelusuario.value = "";
     
@@ -28,6 +33,7 @@ function btnEncriptar(){
     miniinfo.style.display = "block";
 }
 function btnDesencriptar(){
+    
     const textDesencriptado = Desencriptar(txtdelusuario.value.replace(regex, ""));
     mensaje.value = textDesencriptado;
     txtdelusuario.value = "";
@@ -36,6 +42,19 @@ function btnDesencriptar(){
     spam.style.display = "none";
     miniinfo.style.display = "block";
 }
+function btnCopiar(){
+    // Selecciona el texto en el elemento
+    mensaje.select();
+    // Para dispositivos móviles
+    mensaje.setSelectionRange(0, 99999); 
+
+    // Copia el texto seleccionado al portapapeles
+    document.execCommand('copy');
+
+    // para que muestre una alerta cuando se copie el texto del id mensaje
+    alert('Texto copiado: ' + mensaje.value);
+}
+
 //funciones encriptar y descencriptar
 function Encriptar(stringEncriptado){
      
@@ -83,4 +102,4 @@ function Desencriptar(stringDesencriptado){
 
     return stringDesencriptado;
 }
-// spam.style.display = "none";
+
